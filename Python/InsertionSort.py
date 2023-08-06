7abed058e4e5be841f18586a371d4e4235bc9e23
izabelabarbara <134167905+izabelabarbara@users.noreply.github.com>
2023-08-06 14:27:56 +0200

    Update InsertionSort.py

--- Python/InsertionSort.py
+++ Python/InsertionSort.py
@@ -1,12 +1,10 @@
 def insert(array, rightIndex, value):
-    i = rightIndex
-    while i >= 0 and value < array[i]:
-        array[i + 1] = array[i]
-        i -= 1
-    array[i + 1] = value
-    return array
+    j = rightIndex
+    while j >= 0 and array[j] > value:
+        array[j + 1] = array[j]
+        j -= 1
+    array[j + 1] = value
 
-# some test below
-array = [3, 5, 7, 11, 13, 2, 9, 6]
-insert(array, 4, 2)
-print("Array after inserting 2: ", array)
+def insertionSort(array):
+    for i in range(1, len(array)):
+        insert(array, i - 1, array[i])
