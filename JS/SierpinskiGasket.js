ba678be0229945e7538fd8a323d2cd084ac1e7c9
izabelabarbara <134167905+izabelabarbara@users.noreply.github.com>
2023-08-07 13:40:26 +0200

    Create SierpinskiGasket.js

---
+++ JS/SierpinskiGasket.js
@@ -0,0 +1,28 @@
+// not my code
+// code from KhanAcademy:
+// https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/the-sierpinksi-gasket
+
+var dim = 240;
+var minSize = 8;
+	
+var drawGasket = function(x, y, dim) {
+    if (dim <= minSize) {
+	    rect(x, y, dim, dim);
+    }
+    else {
+	    var newDim = dim / 2;
+	    //drawGasket(x, y, newDim);
+	    drawGasket(x + newDim, y, newDim);
+	    drawGasket(x, y + newDim, newDim);
+	    drawGasket(x + newDim, y + newDim, newDim);
+    }
+};
+
+draw = function() {
+    background(255, 255, 255);
+    fill(255, 255, 0);
+    rect(0, 0, dim, dim);
+    stroke(0, 0, 255);
+    fill(0, 0, 255);
+    drawGasket(0, 0, dim);
+};
