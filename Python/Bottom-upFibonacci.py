920334907e3f881e0d5504223b3dfbd851a87bc5
izabelabarbara <134167905+izabelabarbara@users.noreply.github.com>
2023-08-07 23:38:38 +0200

    Update Bottom-upFibonacci.py

--- Python/Bottom-upFibonacci.py
+++ Python/Bottom-upFibonacci.py
@@ -7,11 +7,11 @@ def fib(n):
     twoBehind = 0 # for the result of n-1
     oneBehind = 1 # for the result of n-2
     result = 0 
-  for i in range (n-1):
-    result = oneBehind + twoBehind # update the result
-    twoBehind = oneBehind # update 
-    oneBehind = result # update 
-  return result
+    for i in range (n-1):
+      result = oneBehind + twoBehind # update the result
+      twoBehind = oneBehind # update 
+      oneBehind = result # update 
+    return result
 
 for j in range (0,10):
   print (fib(j))
