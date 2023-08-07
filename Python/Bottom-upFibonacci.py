23304ad114d592e83b3ef1a602baa21b64784c6e
izabelabarbara <134167905+izabelabarbara@users.noreply.github.com>
2023-08-07 23:16:03 +0200

    Create Bottom-upFibonacci.py

---
+++ Python/Bottom-upFibonacci.py
@@ -0,0 +1,17 @@
+# bottom-up fibonacci
+
+def fib(n):
+  if n == 0 or n == 1:
+    return n
+  else:
+    twoBehind = 0 # for the result of n-1
+    oneBehind = 1 # for the result of n-2
+    result = 0 
+  for i in range (n-1):
+    result = oneBehind + twoBehind # update the result
+    twoBehind = oneBehind # update 
+    oneBehind = result # update 
+  return result
+
+for j in range (0,10):
+  print (fib(j))
